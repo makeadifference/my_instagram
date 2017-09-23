edfc5acdc96844ee17e8f578dcf573064b44c615
drf <drf@drfdeiMac.local>
2017-09-23 17:52:51 +0800

    a app like intagram by myself

---
+++ commentCell.swift
@@ -0,0 +1,24 @@
+//
+//  commentCell.swift
+//  simple
+//
+//  Created by drf on 2017/9/19.
+//  Copyright © 2017年 drf. All rights reserved.
+//
+
+import UIKit
+
+class commentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ simple/newsCell.swift
@@ -0,0 +1,24 @@
+//
+//  newsCell.swift
+//  simple
+//
+//  Created by drf on 2017/9/23.
+//  Copyright © 2017年 drf. All rights reserved.
+//
+
+import UIKit
+
+class newsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
